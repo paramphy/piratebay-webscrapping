1ea6ce00894d7bf6128a703c84f3da1e1621cd46
Paramesh Chandra <parameshchandra28@gmail.com>
2021-04-24 17:53:52 +0530

    update

--- sotnikam_web_scraping.py
+++ sotnikam_web_scraping.py
@@ -13,28 +13,25 @@ def upload_status(URL = "https://officialpiratebay.com/search.php?q=user:sotnika
     download_list_new = []
 
     download_list = []
+    print(soup.prettify())
 
-    for row in soup.findAll('span',attrs = {'class':'list-item item-name item-title'}):
+    for row in soup.findAll('span',attrs = {'class':['list-item item-name item-title']}):
 
-        quote = {}
-        quote['title'] = row.string
-        download_list.append(quote)
+        title = {}
+        title['title'] = row.get_text()
+        download_list.append(title)
 
-    if not (download_list_new==download_list):
-        print("New content has been uploaded \n")
-
-    download_list_new = download_list
+        
+    print(download_list)
 
     filename = 'sotnikam_page_info.csv'
     print(download_list)
     with open(filename, 'w', newline='') as f:
-        w = csv.DictWriter(f,['title','date'])
+        w = csv.DictWriter(f,['title'])
         w.writeheader()
         for quote in download_list:
             w.writerow(quote)
 
 
-while True:
-    upload_status()
-    time.sleep(60*60)
+upload_status()
 
