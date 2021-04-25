9b2295a2142ad4637397175590e0deac7c2b0be6
Paramesh Chandra <parameshchandra28@gmail.com>
2021-04-25 17:55:36 +0530

    update

--- sotnikam_web_scraping.py
+++ sotnikam_web_scraping.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import time
+import re
 
 def upload_status(URL = "https://officialpiratebay.com/search.php?q=user:sotnikam"):
     
@@ -10,28 +11,58 @@ def upload_status(URL = "https://officialpiratebay.com/search.php?q=user:sotnika
   
     soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
 
-    download_list_new = []
+    title_list = []
+    date_list = []
+    size_list = []
+    link_list = []
+    hash_list = []
+    count = 0
+    #print(soup.prettify())
 
-    download_list = []
-    print(soup.prettify())
+    for title in soup.findAll('span',attrs = {'class':['list-item item-name item-title']}):
 
-    for row in soup.findAll('span',attrs = {'class':['list-item item-name item-title']}):
+        title = title.get_text()
+        title_list.append(title)
 
-        title = {}
-        title['title'] = row.get_text()
-        download_list.append(title)
+    for date in soup.findAll('span',attrs = {'class':[ 'list-item item-uploaded']}):
 
+        date = date.get_text()
+        date_list.append(date)
+
+    for size in soup.findAll('span',attrs = {'class':['list-item item-size']}):
+
+        size = size.get_text()
+        size_list.append(size)
+
+    for link in soup.findAll(href=re.compile("description")):
         
-    print(download_list)
+        
+        link = link.get('href')
+        link = "https://officialpiratebay.com" + str(link)
+        link_list.append(link)
 
-    filename = 'sotnikam_page_info.csv'
-    print(download_list)
-    with open(filename, 'w', newline='') as f:
-        w = csv.DictWriter(f,['title'])
-        w.writeheader()
-        for quote in download_list:
-            w.writerow(quote)
+        r = requests.get(link)
+  
+        soup = BeautifulSoup(r.content, 'html5lib')
 
+        file_hash = soup.findAll(id = "ih")
+    
+        file_hash = file_hash[0].get_text()
+
+        hash_list.append(file_hash)
+
+        count = count + 1
+        print(str(count) + ' no. of HASH found.')
+        
+
+    with open("sotnikam.txt", "w") as f:
+        f.write("Title" + "\t" + "Date" + "\t" + "Size\t" + "Link\t" + "Hash\n" ) 
+        for i in range(len(title_list)):
+            f.write(title_list[i] + '\t' + date_list[i] + '\t' + size_list[i] + '\t' + link_list[i] + '\t' + hash_list[i])
+            f.write('\n')
+    
+    print('Total' + str(count) + " HASH found. File in sotnikam.txt")
 
 upload_status()
+
 
